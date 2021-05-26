92c63747254624bdefbfd4108e2052af0176aa3f
jellyeast <y135768@gmail.com>
2021-05-26 19:19:02 +0800

    v2

--- hello_django/settings.py
+++ hello_django/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -120,10 +122,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 LOGGING = {
     'version': 1,
@@ -143,3 +148,5 @@ LOGGING = {
         },
     },
 }
+
+django_heroku.settings(locals())
